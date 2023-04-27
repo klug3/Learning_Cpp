1cf0826928fa6d86f2f3e06ae8d13bc2d7ed7a73
Uncle <tomasz.madry@amu.edu.pl>
2023-04-27 02:02:17 +0200

    task_3 test file added

---
+++ algorithms_tasks/task_3/task_3_test.cpp
@@ -0,0 +1,12 @@
+#include "task_3.cpp"
+
+int main()
+{
+    cout << "Results:" << endl;
+
+    cout << "Test 1: " << solution(5, 2) << endl;
+    cout << "Test 2: " << solution(8, 3) << endl;
+    cout << "Test 3: " << solution(3, 2) << endl;
+    cout << "Test 4: " << solution(200, 26) << endl;
+    return 0;
+}
