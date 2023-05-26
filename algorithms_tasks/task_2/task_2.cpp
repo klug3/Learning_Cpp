399e5e07d2f59eceaac2a9ab3504558e313891e8
Uncle <tomasz.madry@amu.edu.pl>
2023-05-26 18:28:40 +0200

    init

---
+++ algorithms_tasks/task_2/task_2.cpp
@@ -0,0 +1,19 @@
+/*
+You are given a string S consisting of N letters 'a' and/or 'b'. In one move, you can swap
+one letter for the other ('a' for 'b' or 'b' for 'a').
+Write a function solution that, given such a string S, returns the minimum number of moves
+required to obtain a string containing no instances of three identical consecutive letters.
+*/
+
+#include <iostream>
+#include <vector>
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+
+vector<int> solution(vector<string> &S)
+{
+    // Implement your solution here
+}
